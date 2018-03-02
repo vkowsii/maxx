e6e6258b7d20654b4c6e0eeb3f856b83e002f193
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-03-02 16:26:09 +0530

    Create maxx.c

---
+++ maxx.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],i,b=0,x;
+    printf("enter the value");
+    scanf("%d",&x);
+    for(i=0;i,=x;i++)
+    {
+        scanf("%d",&a[1]);
+    }
+    b=a[i]
+    for(i=0;i<=x;i++)
+    {
+        if(b<a[i])
+        {
+            b=a[i];
+        }
+        printf("%d",&b);
+        }
+    }
+    
